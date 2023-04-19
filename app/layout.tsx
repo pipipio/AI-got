88921415af480bc18aa2cff5fddd891037eb65ec
pipipio <101705911+pipipio@users.noreply.github.com>
2023-04-19 18:08:08 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,10 +7,10 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "Got-Ai",
   description: "Your personal ChatGPT Chat Bot.",
   appleWebApp: {
-    title: "ChatGPT Next Web",
+    title: "Got-Ai",
     statusBarStyle: "default",
   },
   themeColor: "#fafafa",
